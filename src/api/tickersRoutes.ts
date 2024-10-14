84f89b711c577d381d6de9d7a1807de0fa1e9f85
anas12312 <anashesham000@gmail.com>
2024-10-14 16:09:27 +0300

    anas

--- src/api/tickersRoutes.ts
+++ src/api/tickersRoutes.ts
@@ -10,6 +10,10 @@ tickersRoutes.post('/activate/:ticker', tickersController.activateAlerts)
 tickersRoutes.post('/deactivate/:ticker', tickersController.deactivateAlerts)
 tickersRoutes.post('/intrade/:ticker', tickersController.intrade)
 tickersRoutes.post('/detrade/:ticker', tickersController.detrade)
+tickersRoutes.post('/notify/:ticker', tickersController.notify)
+tickersRoutes.post('/unotify/:ticker', tickersController.unotify)
+tickersRoutes.post('/notify-all', tickersController.notifyAll)
+tickersRoutes.post('/unotify-all', tickersController.unotifyAll)
 tickersRoutes.delete('/:ticker', tickersController.remove)
 
 export default tickersRoutes
